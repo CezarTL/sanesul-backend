e7ba4933158ab007a96efd6030e6a15e6aee3fe0
CezarTL <cezar.icarus@gmail.com>
2026-01-13 13:37:35 -0400

    Update database.py

--- app/database.py
+++ app/database.py
@@ -2,7 +2,7 @@ import os
 import psycopg2
 
 def get_conn():
-    db_url = os.getenv("DATABASE_URL")
-    if not db_url:
-        raise Exception("DATABASE_URL não definida")
-    return psycopg2.connect(db_url)
+    return psycopg2.connect(
+        os.getenv("DATABASE_URL"),
+        sslmode="require"
+    )
