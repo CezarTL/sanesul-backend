0a2e07686ca65977d31806754295c55e88c2985a
CezarTL <cezar.icarus@gmail.com>
2026-01-13 10:07:45 -0400

    Update database.py

--- app/database.py
+++ app/database.py
@@ -1,7 +1,8 @@
 import os
 import psycopg2
 
-DATABASE_URL = os.getenv("DATABASE_URL")
-
 def get_conn():
-    return psycopg2.connect(DATABASE_URL)
+    db_url = os.getenv("DATABASE_URL")
+    if not db_url:
+        raise Exception("DATABASE_URL não definida")
+    return psycopg2.connect(db_url)
